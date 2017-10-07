d0d997a89bbd87893afaa922a8ef5b54e71bcd63
Leonardo Pérez Marulanda <leonardo.perez@yuxigloblal.com>
2017-10-06 19:52:48 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animateState', [
+      transition('* => round', [animate('800ms ease-out', style({ borderRadius: '50%' }))]),
+      transition('* => rotate', [animate('2s ease-out', style({ transform: 'rotateX(360deg)' }))]),
+      transition('* => hinge', [animate('2s easeInElastic', style({ transform: 'rotate(90deg)', transformOrigin: '10% 10%' }))]),
+      transition('* => crazy', [animate('1s ease-out', style({ transform: 'rotateY(360deg)' }))])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app';
+
+  currentState: string;
+
+  updateState(newState) {
+    console.log(newState)
+    this.currentState = newState;
+  }
+
 }
