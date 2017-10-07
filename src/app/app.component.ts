67154979cf72cb2e5b3ac25b6731df758591e6fb
Leonardo Pérez Marulanda <leonardo.perez@yuxigloblal.com>
2017-10-06 20:40:41 -0500

    still TBD 'bye' animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from "@angular/animations";
+import { trigger, state, style, animate, transition, keyframes  } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,15 @@ import { trigger, state, style, animate, transition } from "@angular/animations"
     trigger('animateState', [
       transition('* => round', [animate('800ms ease-out', style({ borderRadius: '50%' }))]),
       transition('* => rotate', [animate('2s ease-out', style({ transform: 'rotateX(360deg)' }))]),
-      transition('* => hinge', [animate('2s easeInElastic', style({ transform: 'rotate(90deg)', transformOrigin: '10% 10%' }))]),
-      transition('* => crazy', [animate('1s ease-out', style({ transform: 'rotateY(360deg)' }))])
+      transition('* => hinge', [animate(2000,  keyframes([
+        style({ transform: 'rotate(70deg)', transformOrigin: '0 0', offset: 0.3 }),
+        style({ transform: 'rotate(-5deg)', transformOrigin: '0 0', offset: 0.6}),
+        style({ transform: 'rotate(70deg)', transformOrigin: '0 0', offset: 1 })
+        ])
+      )]
+    ),
+      transition('* => crazy', [animate(3000, style({ transform: 'rotate(720deg) scale3d(0.7, 1.5, 0.5)' }))]),
+      transition('* => bye', [animate('800ms ease-out', style({ opacity: '0' }))])
     ])
   ]
 })
