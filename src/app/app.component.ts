b271b97d91d4f135f2753e8798bf004f215d5eb3
Leonardo Pérez M <leono286@gmail.com>
2017-10-07 12:09:38 -0500

    added animation to fadeout elements when hovered the bye button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes  } from "@angular/animations";
+import { trigger, state, style, animate, transition, keyframes, query, stagger } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,23 @@ import { trigger, state, style, animate, transition, keyframes  } from "@angular
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('animateState', [
+      state('bye', style({ opacity: '0' })),
       transition('* => round', [animate('800ms ease-out', style({ borderRadius: '50%' }))]),
       transition('* => rotate', [animate('2s ease-out', style({ transform: 'rotateX(360deg)' }))]),
-      transition('* => hinge', [animate(2000,  keyframes([
+      transition('* => hinge', [animate(2000, keyframes([
         style({ transform: 'rotate(70deg)', transformOrigin: '0 0', offset: 0.3 }),
-        style({ transform: 'rotate(-5deg)', transformOrigin: '0 0', offset: 0.6}),
+        style({ transform: 'rotate(-5deg)', transformOrigin: '0 0', offset: 0.6 }),
         style({ transform: 'rotate(70deg)', transformOrigin: '0 0', offset: 1 })
-        ])
+      ])
       )]
-    ),
-      transition('* => crazy', [animate(3000, style({ transform: 'rotate(720deg) scale3d(0.7, 1.5, 0.5)' }))]),
-      transition('* => bye', [animate('800ms ease-out', style({ opacity: '0' }))])
+      ),
+      transition('* => crazy', [animate(3000, style({ transform: 'rotate(720deg) scale3d(0.7, 1.5, 0.5)' }))])
+    ]),
+    trigger('hideElements', [
+      state('bye', style({ opacity: '0' })),
+      transition('* => bye', [query('.element-to-hide',  stagger(800, [
+        animate('0.5s', style({ opacity: 0 }))
+      ]))])
     ])
   ]
 })
@@ -25,10 +31,18 @@ export class AppComponent {
   title = 'app';
 
   currentState: string;
+  finalState: string;
 
   updateState(newState) {
     console.log(newState)
     this.currentState = newState;
   }
+
+  hideElements(){
+    console.log('hiding-elements');
+    this.finalState = 'bye';
+  }
+
+
 
 }
